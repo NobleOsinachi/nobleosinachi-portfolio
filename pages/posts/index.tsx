79cd0e51eb2555c53118d364e3b8b401f91276e7
Noble Osinachi <nobleosinachi98@gmail.com>
2023-07-20 12:41:35 +0100

    remove unneeded pages

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,6 +1,37 @@
 export default function Post(){
 return (
-<>Posts</>
+<>Posts
+
+
+
+
+# Photos
+
+Here's some projects I have worked on
+
+import Image from 'next/image'
+
+<Image
+  src="/images/photo2.jpg"
+  alt="Photo"
+  width={1125}
+  height={750}
+  priority
+  className="next-image"
+/>
+[Unsplash ↗ ](https://unsplash.com/photos/WeYamle9fDM)
+
+<Image
+  src="/images/photo.jpg"
+  alt="Photo"
+  width={1125}
+  height={750}
+  priority
+  className="next-image"
+/>
+[Unsplash ↗ ](https://unsplash.com/photos/ndN00KmbJ1c)
+
+</>
 
 );
 
